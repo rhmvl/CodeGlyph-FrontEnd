ba57bcb8cfd77c35a17df0b169fbe3c44122d2b6
rhmvl <ramaabdiansyah03@gmail.com>
2025-11-02 15:34:24 +0700

    Load theme from local storage

--- tailwindcss.config.js
+++ tailwindcss.config.js
@@ -1,10 +1,16 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  purge: ['./**/*.{js,ts,jsx,tsx}'],
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx,html}",
-  ],
   darkMode: 'class',
+  theme: {
+    extend: {
+      backgroundImage: {
+        // 'noise-pattern': "url('/textures/noise.png')",
+        'radial-light': 'radial-gradient(circle at 50% 0%, rgba(0,255,255,0.08), transparent 60%)',
+        'radial-dark': 'radial-gradient(circle at 50% 0%, rgba(0,0,0,0.08), transparent 60%)',
+      },
+      animation: {
+        'pulse-slow': 'pulse 4s ease-in-out infinite',
+      },
+    },
+  },
   plugins: [],
 }
